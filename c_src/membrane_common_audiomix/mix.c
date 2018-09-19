afcd77bc73a9afbfcd798158376cda9cf2e5c946
Vladyslav Hlembotskyi <nobikik9@gmail.com>
2018-09-19 15:29:42 +0200

    Added working version for s16le

--- c_src/membrane_common_audiomix/mix.c
+++ c_src/membrane_common_audiomix/mix.c
@@ -1,8 +1,30 @@
-#include "mix.h"
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4")
+#pragma GCC optimize("unroll-loops")
 
-UNIFEX_TERM mix(UnifexEnv* env, UnifexPayload ** buffers, unsigned int n) {
-  UnifexPayload * mix_payload
-  return unifex_payload_to_term(env, buffers[0]);
+#include "mix.h"
+#include <limits.h>
+
+UNIFEX_TERM mix(UnifexEnv* env, UnifexPayload** buffers, unsigned int n) {
+  int sequence_length = buffers[0]->size;
+  UnifexPayload* mix_payload = unifex_payload_alloc(env, buffers[0]->type, sequence_length);
+  for (int i = 0; i < sequence_length; i += 2) {
+    short* now;
+    int sum = 0;
+    for (int j = 0; j < (int) n; ++j) {
+      sum += *((short*)(buffers[j]->data + i));
+    }
+    if (sum < SHRT_MIN) {
+      sum = SHRT_MIN;
+    }
+    if (sum > SHRT_MAX) {
+      sum = SHRT_MAX;
+    }
+    *((short*)(mix_payload->data + i)) = (short) sum;
+  }
+  UNIFEX_TERM result = mix_result(env, mix_payload);
+  unifex_payload_release_ptr(&mix_payload);
+  return result;
 }
 
 void handle_destroy_state(UnifexEnv* env, State* state) {}
--- lib/membrane_common_audiomix/native.ex
+++ lib/membrane_common_audiomix/native.ex
@@ -1,3 +1,14 @@
 defmodule Membrane.Common.AudioMix.Native do
   use Unifex.Loader
+  alias Membrane.Time
+
+  use Membrane.Mixins.Log, tags: :membrane_element_audiomix
+
+  def mix_wrapper(buffers) do
+    start = Time.monotonic_time()
+    buffer = mix(buffers)
+    finish = Time.monotonic_time()
+    info("Mixing time: #{(finish - start) |> Time.to_milliseconds}")
+    buffer
+  end
 end
